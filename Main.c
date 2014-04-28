dd41cc118e8ab3d9459c0ffee01615d1338de443
jchonch <jchonch@users.noreply.github.com>
2014-04-27 19:42:31 -0500

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,38 +1,56 @@
 //*****************************************************************************
 // TITLE:			LAB 8
 // 
-// NAME:		    <Your Name Here>
+// NAME:		    Shu, Ruihao, David, Jim
 // LAB SECTION:		<301, 302, 303, 304, 305, or 306>
-// LAB DAY:			<Lab Day>
+// LAB DAY:			Monday's Eve
 //
 // CLASS:			ECE 210
 // DATE: 			FALL 2014
 // 
-// DESCRIPTION:		<Insert a short description of what you are working on in 
-//                   in this lab>
+// DESCRIPTION:		Bop It!
 //*****************************************************************************
+
 /* Initialization Functions */
+
 void	LEDBARInit(void);
 void 	DIPSWInit(void);
 void	PBSwInit(void);	
 void 	RGB_LEDInit(void);
 void 	sysTickInit(void);
-int rand(void);
-/* Inputs, outputs, and wait timers */			
-int 	read_PBSwitchNum(int SwitchNumber);
-int		read_Switches(void);
-void    LEDBAROutput(int value);
-void 	turnOn(char color);
-void 	turnOff(char color);
-void  potentiometersInit(void);
+
+/*Initializing Potentiometers*/
+void	potentiometersInit();
 int   readPotentiometer0(void);
 int   readPotentiometer1(void);
 int   readPotentiometer2(void);
-void    sysTickWait1mS(int waitTime);
+
 void	RIT128x96x4Init(int freq);
-void    RIT128x96x4Clear(void); 
-void    RIT128x96x4StringDraw(const char* letter, int xx, int yy, int intensity);
-char* 	convert(int baudotCode);
+void  RIT128x96x4Clear(void); 
+void  RIT128x96x4StringDraw(const char* letter, int xx, int yy, int intensity);
+
+/* Inputs, outputs, and wait timers */			
+int 	read_PBSwitchNum(int SwitchNumber);
+int		read_SwitchNum(int SwitchNum);
+int		read_Switches(void);
+void  LEDBAROutput(int value);
+void 	turnOn(char color);
+void 	turnOff(char color);
+void  sysTickWait1mS(int waitTime);
+
+/* Xbee Functions */
+void 	Xbee_ConfigureAddresses(int destination, int ownAddr);
+void 	Xbee_Send(int message);
+int		Xbee_Receive(void);
+void 	Display(int numWin);
+void  RIT128x96x4StringDraw(const char* letter, int xx, int yy, int intensity);
+
+/*Conversion functions*/
+char* convertForWin(int baudotCode);
+char* convert(int baudotCode);
+
+int rand(void); //random number generator
+
 //*****************************************************************************
 //
 // Main Program:
@@ -42,32 +60,48 @@ int
 main(void)
 {
 	//Initializing the variables used in the lab.
+	int turn = 1;			//Pushbutton variables				
+	int p1Add = 1;		//Address P1	
+	int p2Add = 2;		//Address P2
+	int p3Add = 3;		//Address P3
+	int p4Add = 4;		//Address P4
+	int p1Dead = 0;		
+	int p2Dead = 0;
+	int p3Dead = 0;
+	int p4Dead = 0;
+	int p1DeadPrev;		//Contains the previous value of p1Dead
+	int p2DeadPrev;		//Contains prev value of p2Dead
+	int p3DeadPrev;		//Contains prev value of p3Dead
+	int p4DeadPrev;		//Contains prev value of p4Dead
+	int currAdd;
+	int nxtAdd;
+	int countDead = 0;
 	int turnItCodes[10];
 	int correctcode[10];
 	int pushItCodes[10];
 	int switchItCodes[10];
 	int loserCodes[10];
-	int dead;
+		
 	int waitTimer = 0;
 	int waitCounter = 0;
-	int waitLimit = 100;
+	int waitLimit = 5;
 	
 	int jj; 						// number of turns counter
 	int ii;   					//for counter variable
 	int xx;							//x axis string position
 	int yy;							//y axis string position
-	
+
 	int turnItMsg;		//holds 'Turn It' message
 	int correctMsg;	// holds 'Correct' message
 	int pushItMsg;	//  holds 'Push it" message
 	int loserMsg; //holds "you lose' message
 	int switchItMsg;
-	
+
 	// used to read current values
 	int potenValue0 = 0;
 	int pushButton1 = 1;
 	int switchButton1 = 0;
-	
+
 	int isInput = 0;
 	int inputRequested = 0;
 	int inputSelected = 0;
@@ -76,7 +110,7 @@ main(void)
 	int number = 0;																//??????????????????????
 	int numberOfTurns = 4;
 	int SwitchNum;
-								
+
 	//Initializing the LEDBAR, RGB LED, DIPSwitches and Pushbuttons, and a wait timer
 	LEDBARInit();
 	DIPSWInit();
@@ -85,45 +119,45 @@ main(void)
 	sysTickInit();
 	potentiometersInit();
 	RIT128x96x4Init(1000000);
-
-	// code for 'Turn It'
-  turnItCodes[0] = 16;	// T
-	turnItCodes[1] = 7;	// U
+	
+	//Initialize display arrays
+	turnItCodes[0] = 16;	// T
+	turnItCodes[1] = 7;		// U
 	turnItCodes[2] = 10;	// R
 	turnItCodes[3] = 12;	// N
-	turnItCodes[4] = 4;	// 
-	turnItCodes[5] = 6;	// I
+	turnItCodes[4] = 4;		// 
+	turnItCodes[5] = 6;		// I
 	turnItCodes[6] = 16;	// T
-	
+
 		// code for 'Switch It'
-	switchItCodes[0] = 5;	// S
-	switchItCodes[1] = 19; // W
-	switchItCodes[2] = 6; // I
+	switchItCodes[0] = 5;		// S
+	switchItCodes[1] = 19;	// W
+	switchItCodes[2] = 6; 	// I
 	switchItCodes[3] = 16;	// T
-	switchItCodes[4] = 14; // C
-	switchItCodes[5] = 20; // H
-	switchItCodes[6] = 4;	//
-	switchItCodes[7] = 6;	// I
+	switchItCodes[4] = 14;	// C
+	switchItCodes[5] = 20;	// H
+	switchItCodes[6] = 4;		//
+	switchItCodes[7] = 6;		// I
   switchItCodes[8] = 16;	// T
-	
+
 	// code for 'Correct'
 	correctcode[0] = 14;	// C
 	correctcode[1] = 24;	// O
 	correctcode[2] = 10;	// R
 	correctcode[3] = 10;	// R
-	correctcode[4] = 1;	// E
+	correctcode[4] = 1;		// E
 	correctcode[5] = 14;	// C
 	correctcode[6] = 16;	// T 
-	
+
 	// code for 'Push It'
 	pushItCodes[0] = 22;	// P
-	pushItCodes[1] = 7;	// U
-	pushItCodes[2] = 5;	// S
+	pushItCodes[1] = 7;		// U
+	pushItCodes[2] = 5;		// S
 	pushItCodes[3] = 20;	// H
-	pushItCodes[4] = 4;	// 
-	pushItCodes[5] = 6;	// I
+	pushItCodes[4] = 4;		// 
+	pushItCodes[5] = 6;		// I
 	pushItCodes[6] = 16;	// T 	
-	
+
 	// code for 'You Lose'
 	loserCodes[0] = 21;	//Y
 	loserCodes[1] = 24;	//O
@@ -134,37 +168,94 @@ main(void)
 	loserCodes[6] = 5;	//S
 	loserCodes[7] = 1;	//E
 	
-	//Flash to say we are getting ready to go!
-	turnOn('B');
-	sysTickWait1mS(250);
-	turnOff('B');
-
+	countDead = p1Dead + p2Dead + p3Dead + p4Dead;
+	
 	//Clear messages on the screen
 	RIT128x96x4Clear();
-	
+
 	//Start at the upper left hand corner of the screen!
 	xx = 0;
 	yy = 0;
 	
+	//Wait to configure address
+	turnOn('R');
+	sysTickWait1mS(150);
+	turnOff('R');
+	sysTickWait1mS(150);
+	turnOn('R');
+	sysTickWait1mS(150);
+	turnOff('R');
+	sysTickWait1mS(150);
+
+	Xbee_ConfigureAddresses(p2Add, p1Add);											//Board Specific
 	
+	//LED Sequence lets us know we have configured the Xbee 
+	turnOn('B');
+	sysTickWait1mS(150);
+	turnOff('B');
+	sysTickWait1mS(150);
+	turnOn('B');
+	sysTickWait1mS(150);
+	turnOff('B');
+	sysTickWait1mS(150);
 	
-	
-	
-	
-	
-	
-	
-	while(1){
-	
+	Xbee_Send(turn);	
+	Xbee_Send(countDead);
+	Xbee_Send(p1Dead);
+	Xbee_Send(p2Dead);
+	Xbee_Send(p3Dead);
+	Xbee_Send(p4Dead);
 		
-  while(dead == 0){
+	/* Main Part of the Program starts here */
+	while (1){
+		turn = Xbee_Receive();
+		countDead = Xbee_Receive();
 		
+		p1DeadPrev = p1Dead;
+		p1Dead = Xbee_Receive();
+		if (p1Dead == 0x5A5){			//prevents the error code from being assigned to p1Dead
+			p1Dead = p1DeadPrev;
+		}
+		
+		p2DeadPrev = p2Dead;
+		p2Dead = Xbee_Receive();
+		if (p2Dead == 0x5A5){			//prevents the error code from being assigned to p2Dead
+			p2Dead = p2DeadPrev;
+		}
+		
+		p3DeadPrev = p3Dead;
+		p3Dead = Xbee_Receive();	//prevents the error code from being assigned to p3Dead
+		if (p3Dead == 0x5A5){
+			p3Dead = p3DeadPrev;
+		}
+		
+		p4DeadPrev = p4Dead;
+		p2Dead = Xbee_Receive();	//prevents the error code from being assigned to p4Dead
+		if (p4Dead == 0x5A5){
+			p4Dead = p4DeadPrev;
+		}
+		
+				
+		if(turn == 1 && countDead < 3){												//turn is board specific
+	
+			turnOn('G');
+			sysTickWait1mS(150);
+			turnOff('G');
+			sysTickWait1mS(150);
+			turnOn('G');
+			sysTickWait1mS(150);
+			turnOff('G');
+			sysTickWait1mS(150);
+			
+			//Play Game
+			while(p1Dead == 0){ //Board specific
+
 		for(jj = 0 ; jj<numberOfTurns ; jj++)
 		{
-		
+
 			// Assign inputRequest - 1: button,  2: switch, 3: knob 
 		int randomNum = rand() % 3;	// generates a number between 0-3
-		
+
 		if (randomNum <= 1)
 		{
 			inputRequested = 1;	// button
@@ -177,7 +268,7 @@ main(void)
 		{
 			inputRequested = 3;	// knob
 		}
-		
+
 		if (inputRequested == 1)
 		{
 			//Display 'Push it'
@@ -203,7 +294,7 @@ main(void)
 				RIT128x96x4StringDraw(convert(turnItMsg), xx + 6*ii,yy,15);
 			}
 		}
-		
+
       
 			while (isInput == 0)	// loop until an input is detected
 			{
@@ -234,7 +325,7 @@ main(void)
 						ATWCCW = 1;
 					}
 				}
-					
+
 				//Did a switch change?
 					if (switchON == 0)
              {
@@ -254,28 +345,29 @@ main(void)
                      inputSelected = 2;
                  }
          }
-				
+
 				//Was a button pushed?
 				if (pushButton1 == 0)
 				{
 					inputSelected = 1;
 					isInput = 1;
 				}
-				
+
 				//Are we over the time limit?
 				if(waitTimer > waitLimit){
-					dead = 1;                                     //Board Specific!!
+					p1Dead = 1;                                     //Board Specific!!
 					isInput = 1;
 					inputSelected = 0;
 				}
-				
+
 				waitTimer = waitTimer + 1;
-				sysTickWait1mS(10);
-				
+				sysTickWait1mS(500);
+				LEDBAROutput(2^(waitTimer));
+
 			}//end while(no input)
 			waitTimer = 0;
 			RIT128x96x4Clear();
-			
+
 			// Was the input correct?
 			if (inputSelected == inputRequested)
 			{
@@ -283,7 +375,7 @@ main(void)
 					turnOn('G');
 					sysTickWait1mS(250);
 					turnOff('G');
-				
+
 					// Display 'correct'
 					//Letters are displayed left to right
 					for(ii = 0; ii < 7;ii++)
@@ -299,7 +391,7 @@ main(void)
 					turnOn('R');
 					sysTickWait1mS(250);
 					turnOff('R');
-					dead = 1; ////THIS VALUE IS BOARD SPECIFIC
+					p1Dead = 1; ////THIS VALUE IS BOARD SPECIFIC
 					for(ii = 0 ; ii<8 ; ii++)
 					{
 						loserMsg =  loserCodes[ii];
@@ -307,36 +399,69 @@ main(void)
 					}
 					break;
 				}
-					
+
 				//RESET VALUES
 				isInput = 0;
 				inputSelected = 0;
-				
+
 			} //end for-loop number of turns
-		
-				
-				
-				
-				
-				
+
 				turnOn('B'); //meaning its time to pass
-			}//end while(not dead)
+			}//end while(not p1Dead)
 			
+			//Finish turn
 			
-			
-			
-			// send activation signal to next board here??
-			
-			
-			
-		}//end while(1)
-	}// end main
+			countDead = p1Dead + p2Dead + p3Dead + p4Dead;
+	
+			//Send turn
+			Xbee_Send(turn);
+	
+			//Send num dead
+			Xbee_Send(countDead);
+			Xbee_Send(p1Dead);
+			Xbee_Send(p2Dead);
+			Xbee_Send(p3Dead);
+			Xbee_Send(p4Dead);
+	
+		}
+		else if (countDead >= 3) {
+				if (!p1Dead){
+						//Display "P1 Wins!"
+							Display(13);
+							break;
+						}
+				else if (!p2Dead){
+							//Display "P2 Wins!"
+							Display(14);
+							break;
+						}
+				else if (!p3Dead){
+							//Display "P3 Wins!"
+							Display(15);
+							break;
+						}
+				else if (!p4Dead){
+							//Display "P4 Wins!"
+							Display(16);
+							break;
+						}
+				}
+			}
+		}
+	
 	
 
 
-/* The convert() function maps 5 bit Baudot codes to the output needed for 
-   for the LCD Display */
-
+void Display(int numWin){
+		RIT128x96x4StringDraw(convertForWin(22), 3, 15, 15);
+		RIT128x96x4StringDraw(convertForWin(numWin), 9, 15, 15);
+		RIT128x96x4StringDraw(convertForWin(4), 15, 15, 15);
+		RIT128x96x4StringDraw(convertForWin(19), 21, 15, 15);
+		RIT128x96x4StringDraw(convertForWin(6), 27, 15, 15);
+		RIT128x96x4StringDraw(convertForWin(12), 33, 15, 15);
+		RIT128x96x4StringDraw(convertForWin(5), 39, 15, 15);
+		
+}
 
 char* convert(int baudotCode)
 {
@@ -372,3 +497,42 @@ char* convert(int baudotCode)
 	}
 	return " ";
 }
+
+char* convertForWin(int baudotCode)
+{
+	switch(baudotCode)
+	{
+		case(1): return "E";
+		case(3): return "A";
+		case(4): return " ";
+		case(5): return "S";
+		case(6): return "I";
+		case(7): return "U";
+		case(9): return "D";
+		case(10): return "R";
+		case(11): return "J";
+		case(12): return "N";		 
+		/////////////////////////////
+		case(13): return "1";					 	
+		case(14): return "2";
+		case(15): return "3";
+		case(16): return "4";
+		////////////////////////////
+		case(17): return "Z";
+		case(18): return "L";
+		case(19): return "W";
+		case(20): return "H";			 					 
+		case(21): return "Y";
+		case(22): return "P";
+		case(23): return "Q";
+		case(24): return "O";
+		case(25): return "B";
+		case(26): return "G";
+		case(28): return "M";
+		case(29): return "X";
+		case(31): return "V";
+				
+	}
+	return " ";
+}
+
