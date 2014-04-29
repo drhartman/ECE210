88ae945f99a77fa9bb594499c811a71fd08eb508
Anne941 <loo2@wisc.edu>
2014-04-28 19:07:47 -0600

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,10 +1,28 @@
-/* Initialization Functions */
+#include <stdlib.h>		/* required for randomize() and random() */
+#include <time.h>
 
+//*****************************************************************************
+// TITLE:			LAB 8
+// 
+// NAME:		    <Your Name Here>
+// LAB SECTION:		<301, 302, 303, 304, 305, or 306>
+// LAB DAY:			<Lab Day>
+//
+// CLASS:			ECE 210
+// DATE: 			FALL 2014
+// 
+// DESCRIPTION:		<Insert a short description of what you are working on in 
+//                   in this lab>
+//*****************************************************************************
+/* Initialization Functions */
 void	LEDBARInit(void);
 void 	DIPSWInit(void);
 void	PBSwInit(void);	
 void 	RGB_LEDInit(void);
 void 	sysTickInit(void);
+
+/*Initialize Random num generator*/
+int rand(void); 
 
 /*Initializing Potentiometers*/
 void	potentiometersInit();
@@ -12,31 +30,31 @@ int   readPotentiometer0(void);
 int   readPotentiometer1(void);
 int   readPotentiometer2(void);
 
-void	RIT128x96x4Init(int freq);
-void  RIT128x96x4Clear(void); 
-void  RIT128x96x4StringDraw(const char* letter, int xx, int yy, int intensity);
-
 /* Inputs, outputs, and wait timers */			
 int 	read_PBSwitchNum(int SwitchNumber);
 int		read_SwitchNum(int SwitchNum);
-int		read_Switches(void);
+int		read_Switches();
 void  LEDBAROutput(int value);
+void    LEDBAR_TurnOn(int number);
+void    LEDBAR_TurnOff(int number);
 void 	turnOn(char color);
 void 	turnOff(char color);
 void  sysTickWait1mS(int waitTime);
 
 /* Xbee Functions */
 void 	Xbee_ConfigureAddresses(int destination, int ownAddr);
-void 	Xbee_Send(int message);
-int		Xbee_Receive(void);
+void Xbee_SendArray(char * s, int messageLength);
+int Xbee_ReceiveArray(char * t, int messageLength);
+
+/*Display Configurations*/
 void 	Display(int numWin);
+void	RIT128x96x4Init(int freq);
+void  RIT128x96x4Clear(void); 
 void  RIT128x96x4StringDraw(const char* letter, int xx, int yy, int intensity);
 
 /*Conversion functions*/
 char* convertForWin(int baudotCode);
 char* convert(int baudotCode);
-
-int rand(void); //random number generator
 
 //*****************************************************************************
 //
@@ -62,41 +80,50 @@ main(void)
 	int p4DeadPrev;		//Contains prev value of p4Dead
 	int currAdd;
 	int nxtAdd;
+	char messageReceived[5];
+	char sendMessage[5];
+	int errorCode;
 	int countDead = 0;
-	int turnItCodes[10];
-	int correctcode[10];
-	int pushItCodes[10];
-	int switchItCodes[10];
-	int loserCodes[10];
-		
+				
 	int waitTimer = 0;
 	int waitCounter = 0;
 	int waitLimit = 100;
 	
-	int jj; 						// number of turns counter
+	int numTries = 0; 						// number of tries left
 	int ii;   					//for counter variable
 	int xx;							//x axis string position
 	int yy;							//y axis string position
 
-	int turnItMsg;		//holds 'Turn It' message
-	int correctMsg;	// holds 'Correct' message
-	int pushItMsg;	//  holds 'Push it" message
+	int turnIt;		//holds 'Turn It' message
+	int correct;	// holds 'Correct' message
+	int pushIt;	//  holds 'Push it" message
 	int loserMsg; //holds "you lose' message
-	int switchItMsg;
+	int switchIt;
 
 	// used to read current values
-	int potenValue0 = 0;
+		int potenValue0 = 0;
+	int potenValue1 = 0;
+	int potenValue2 = 0;
 	int pushButton1 = 1;
+	int pushButton2 = 1;
+	int pushButton3 = 1;
+	int switchButton0 = 0;
 	int switchButton1 = 0;
+	int switchButton2 = 0;
+	int switchButton3 = 0;
+	int switchButton4 = 0;
 
 	int isInput = 0;
 	int inputRequested = 0;
 	int inputSelected = 0;
-	int ATWCCW = 1; // 0 = false, 1 = true
-	int switchON = 1; // 0 = false, 1= true
-	int number = 0;																//??????????????????????
-	int numberOfTurns = 4;
-	int SwitchNum;
+	int ATWCCW0 = 1; // 0 = false, 1 = true
+	int ATWCCW1 = 1; // 0 = false, 1 = true
+	int ATWCCW2 = 1; // 0 = false, 1 = true
+	int switchON0 = 0; // 0 = false, 1= true
+	int switchON1 = 0; // 0 = false, 1= true
+	int switchON2 = 0; // 0 = false, 1= true
+	int switchON3 = 0; // 0 = false, 1= true
+	int switchON4 = 0; // 0 = false, 1= true
 
 	//Initializing the LEDBAR, RGB LED, DIPSwitches and Pushbuttons, and a wait timer
 	LEDBARInit();
@@ -108,6 +135,7 @@ main(void)
 	RIT128x96x4Init(1000000);
 	
 	//Initialize display arrays
+	int turnItCodes[10];
 	turnItCodes[0] = 16;	// T
 	turnItCodes[1] = 7;		// U
 	turnItCodes[2] = 10;	// R
@@ -116,7 +144,8 @@ main(void)
 	turnItCodes[5] = 6;		// I
 	turnItCodes[6] = 16;	// T
 
-		// code for 'Switch It'
+	// code for 'Switch It'
+	int switchItCodes[10];
 	switchItCodes[0] = 5;		// S
 	switchItCodes[1] = 19;	// W
 	switchItCodes[2] = 6; 	// I
@@ -125,9 +154,10 @@ main(void)
 	switchItCodes[5] = 20;	// H
 	switchItCodes[6] = 4;		//
 	switchItCodes[7] = 6;		// I
-  switchItCodes[8] = 16;	// T
+	switchItCodes[8] = 16;	// T
 
 	// code for 'Correct'
+	int correctcode[10];
 	correctcode[0] = 14;	// C
 	correctcode[1] = 24;	// O
 	correctcode[2] = 10;	// R
@@ -137,6 +167,7 @@ main(void)
 	correctcode[6] = 16;	// T 
 
 	// code for 'Push It'
+	int pushItCodes[10];
 	pushItCodes[0] = 22;	// P
 	pushItCodes[1] = 7;		// U
 	pushItCodes[2] = 5;		// S
@@ -146,6 +177,7 @@ main(void)
 	pushItCodes[6] = 16;	// T 	
 
 	// code for 'You Lose'
+	int loserCodes[10];
 	loserCodes[0] = 21;	//Y
 	loserCodes[1] = 24;	//O
 	loserCodes[2] = 7;	//U
@@ -155,7 +187,8 @@ main(void)
 	loserCodes[6] = 5;	//S
 	loserCodes[7] = 1;	//E
 	
-	countDead = p1Dead + p2Dead + p3Dead + p4Dead;
+	//for the random number generator
+	int SEED;
 	
 	//Clear messages on the screen
 	RIT128x96x4Clear();
@@ -174,7 +207,8 @@ main(void)
 	turnOff('R');
 	sysTickWait1mS(150);
 
-	Xbee_ConfigureAddresses(p2Add, p1Add);											//Board Specific
+	Xbee_ConfigureAddresses(p2Add, p1Add);														//Board Specific
+	RIT128x96x4Clear();
 	
 	//LED Sequence lets us know we have configured the Xbee 
 	turnOn('B');
@@ -186,44 +220,30 @@ main(void)
 	turnOff('B');
 	sysTickWait1mS(150);
 	
-	Xbee_Send(turn);	
-	Xbee_Send(countDead);
-	Xbee_Send(p1Dead);
-	Xbee_Send(p2Dead);
-	Xbee_Send(p3Dead);
-	Xbee_Send(p4Dead);
+	sendMessage[0] = (char) (p1Dead + 48);
+	sendMessage[1] = (char) (p2Dead + 48);
+	sendMessage[2] = (char) (p3Dead + 48);
+	sendMessage[3] = (char) (p4Dead + 48);
+	sendMessage[4] = (char) (1 + 48);
+	
+	Xbee_SendArray(sendMessage, 5);
 		
 	/* Main Part of the Program starts here */
 	while (1){
-		turn = Xbee_Receive();
-		countDead = Xbee_Receive();
+		errorCode =  Xbee_ReceiveArray(messageReceived, 5);
 		
-		p1DeadPrev = p1Dead;
-		p1Dead = Xbee_Receive();
-		if (p1Dead == 0x5A5){			//prevents the error code from being assigned to p1Dead
-			p1Dead = p1DeadPrev;
+		if (errorCode != 0x5A5){
+			
+			p1Dead = (int) messageReceived[0] - (int) '0';
+			p2Dead = (int) messageReceived[1] - (int) '0';
+			p3Dead = (int) messageReceived[2] - (int) '0';
+			p4Dead = (int) messageReceived[3] - (int) '0';
+			turn = (int) messageReceived[4] - (int) '0';
 		}
 		
-		p2DeadPrev = p2Dead;
-		p2Dead = Xbee_Receive();
-		if (p2Dead == 0x5A5){			//prevents the error code from being assigned to p2Dead
-			p2Dead = p2DeadPrev;
-		}
+		countDead = p1Dead + p2Dead + p3Dead + p4Dead;
 		
-		p3DeadPrev = p3Dead;
-		p3Dead = Xbee_Receive();	//prevents the error code from being assigned to p3Dead
-		if (p3Dead == 0x5A5){
-			p3Dead = p3DeadPrev;
-		}
-		
-		p4DeadPrev = p4Dead;
-		p2Dead = Xbee_Receive();	//prevents the error code from being assigned to p4Dead
-		if (p4Dead == 0x5A5){
-			p4Dead = p4DeadPrev;
-		}
-		
-				
-		if(turn == 1 && countDead < 3){												//turn is board specific
+		if(turn == 1 && countDead < 3){																					//turn is board specific
 	
 			turnOn('G');
 			sysTickWait1mS(150);
@@ -234,125 +254,241 @@ main(void)
 			turnOff('G');
 			sysTickWait1mS(150);
 			
+			/*******************************************************************/
+			
+			
 			//Play Game
-			while(p1Dead == 0){ //Board specific
+			
+			
+			/*******************************************************************/
+			
+			while(p1Dead == 0 && numTries < 5){																			//Board specific
 
-		for(jj = 0 ; jj<numberOfTurns ; jj++)
-		{
-
-			// Assign inputRequest - 1: button,  2: switch, 3: knob 
+		// Assign inputRequest - 1: button,  2: switch, 3: knob 
 		int randomNum = rand() % 3;	// generates a number between 0-3
+		
+		switch(randomNum)
+{
+   case 0:
+      inputRequested = 1;	// button
+      break;
 
-		if (randomNum <= 1)
-		{
-			inputRequested = 1;	// button
-		}
-		else if (1 < randomNum <= 2)
-		{
-			inputRequested = 2;	// switch
-		} 
-		else if (2 < randomNum <= 3)
-		{
-			inputRequested = 3;	// knob
-		}
+   case 1:
+      inputRequested = 2;	// switch
+      break;
 
+   case 2:
+      inputRequested = 3;	// knob
+      break;
+
+}
+		
 		if (inputRequested == 1)
 		{
 			//Display 'Push it'
 			for(ii = 0; ii < 7;ii++)
 			{
-				pushItMsg = pushItCodes[ii];
-				RIT128x96x4StringDraw(convert(pushItMsg), xx + 6*ii,yy,15);
+				pushIt = pushItCodes[ii];
+				RIT128x96x4StringDraw(convert(pushIt), xx + 6*ii,yy,15);
 			}
 		} else if (inputRequested == 2)
 		{
-				//Display 'Switch it'
-			for(ii = 0; ii < 9;ii++)
+			//Display 'Switch it'
+            		for(ii = 0; ii < 9;ii++)
 			{
-				switchItMsg = switchItCodes[ii];
-				RIT128x96x4StringDraw(convert(switchItMsg), xx + 6*ii,yy,15);
+				switchIt = switchItCodes[ii];
+				RIT128x96x4StringDraw(convert(switchIt), xx + 6*ii,yy,15);
 			}	
 		} else if (inputRequested == 3)
 		{
 			//Display 'Turn it'
 			for(ii = 0; ii < 7;ii++)
 			{
-				turnItMsg = turnItCodes[ii];
-				RIT128x96x4StringDraw(convert(turnItMsg), xx + 6*ii,yy,15);
+				turnIt = turnItCodes[ii];
+				RIT128x96x4StringDraw(convert(turnIt), xx + 6*ii,yy,15);
 			}
 		}
 
-      
+    // Set timeout
+		LEDBAROutput(31);
+		
 			while (isInput == 0)	// loop until an input is detected
 			{
 
-
 				// record values of inputs here
-				potenValue0 = readPotentiometer0();				  // knob value
-				pushButton1 = read_PBSwitchNum(1);					// push button
-				switchButton1 = read_SwitchNum(SwitchNum); 	// switch
-
-
+				// knob values
+				potenValue0 = readPotentiometer0();				 
+				potenValue1 = readPotentiometer1();				  
+				potenValue2 = readPotentiometer2();	
+				// switches
+				switchButton0 = read_SwitchNum(0);  
+				switchButton1 = read_SwitchNum(1);  
+				switchButton2 = read_SwitchNum(2);  
+				switchButton3 = read_SwitchNum(3);  
+				switchButton4 = read_SwitchNum(4);
+	      // push buttons			  
+				pushButton1 = read_PBSwitchNum(1);
+        pushButton2 = read_PBSwitchNum(2);
+				pushButton3 = read_PBSwitchNum(3);				
+			    
+				
 				// Did the knob change?
-				if (ATWCCW == 1)
+				// knob0
+				if (ATWCCW0 == 1 && (potenValue0 >= 500))
 				{
-					if (potenValue0 >= 500)
-					{
 						inputSelected = 3;
-						ATWCCW = 0;
+						ATWCCW0 = 0;
 						isInput = 1;
-					}
 				}
-				else	//ATWCW
+				else if (ATWCCW0 == 0 && (potenValue0 <= 500)) //ATWCW
 				{
-					if (potenValue0 <= 500)
-					{
-						isInput = 1;
-						inputSelected = 3;
-						ATWCCW = 1;
-					}
+					isInput = 1;
+					inputSelected = 3;
+					ATWCCW0 = 1;
 				}
+				// knob1
+				if (ATWCCW1 == 1 && (potenValue1 >= 500))
+				{
+						inputSelected = 3;
+						ATWCCW1 = 0;
+						isInput = 1;
+				}
+				else if (ATWCCW1 == 0 && (potenValue1 <= 500)) //ATWCW
+				{
+					isInput = 1;
+					inputSelected = 3;
+					ATWCCW1 = 1;
+				}
+				// knob2
+				if (ATWCCW2 == 1 && (potenValue2 >= 500))
+				{
+						inputSelected = 3;
+						ATWCCW2 = 0;
+						isInput = 1;
+				}
+				else if (ATWCCW2 == 0 && (potenValue2 <= 500)) //ATWCW
+				{
+					isInput = 1;
+					inputSelected = 3;
+					ATWCCW2 = 1;
+				}
+				
 
 				//Did a switch change?
-					if (switchON == 0)
-             {
-                 if (switchButton1 == 1)
-                 {
-                     inputSelected = 2;
-                     switchON = 1;
-                     isInput = 1;
-                 }
-             }
-             else	//switchON
-             {
-                 if (switchButton1 == 0)
-                 {
-                     switchON = 0;
-                     isInput = 1;
-                     inputSelected = 2;
-                 }
-         }
-
+				// switch0
+				if (switchON0 == 0 && switchButton0 == 1) // switchOn holds last switch value 
+        {
+						inputSelected = 2;
+						switchON0 = 1;
+						isInput = 1;
+        } else if (switchON0 == 1 && switchButton0 == 0) 	//switchON
+          {
+             switchON0 = 0;
+             isInput = 1;
+             inputSelected = 2;
+          }
+				// switch1
+				if (switchON1 == 0 && switchButton1 == 1) // switchOn holds last switch value 
+        {
+						inputSelected = 2;
+						switchON1 = 1;
+						isInput = 1;
+        } else if (switchON1 == 1 && switchButton1 == 0) 	//switchON
+          {
+             switchON1 = 0;
+             isInput = 1;
+             inputSelected = 2;
+          }
+				// switch2
+				if (switchON2 == 0 && switchButton2 == 1) // switchOn holds last switch value 
+        {
+						inputSelected = 2;
+						switchON2 = 1;
+						isInput = 1;
+        } else if (switchON2 == 1 && switchButton2 == 0) 	//switchON
+          {
+             switchON2 = 0;
+             isInput = 1;
+             inputSelected = 2;
+          }
+				// switch3
+				if (switchON3 == 0 && switchButton3 == 1) // switchOn holds last switch value 
+        {
+						inputSelected = 2;
+						switchON3 = 1;
+						isInput = 1;
+        } else if (switchON3 == 1 && switchButton3 == 0) 	//switchON
+          {
+             switchON3 = 0;
+             isInput = 1;
+             inputSelected = 2;
+          }
+				// switch4
+				if (switchON4 == 0 && switchButton4 == 1) // switchOn holds last switch value 
+        {
+						inputSelected = 2;
+						switchON4 = 1;
+						isInput = 1;
+        } else if (switchON4 == 1 && switchButton4 == 0) 	//switchON
+          {
+             switchON4 = 0;
+             isInput = 1;
+             inputSelected = 2;
+          }
+        		 	
+					
 				//Was a button pushed?
+			  // push button1
 				if (pushButton1 == 0)
 				{
 					inputSelected = 1;
 					isInput = 1;
 				}
-
+				// push button2
+				if (pushButton2 == 0)
+				{
+					inputSelected = 1;
+					isInput = 1;
+				}
+				// push button3
+				if (pushButton3 == 0)
+				{
+					inputSelected = 1;
+					isInput = 1;
+				}
+				
 				//Are we over the time limit?
 				if(waitTimer > waitLimit){
-					p1Dead = 1;                                     //Board Specific!!
+					p1Dead = 1;   																															 //Board Specific!!
 					isInput = 1;
 					inputSelected = 0;
 				}
 
 				waitTimer = waitTimer + 1;
-				sysTickWait1mS(10);
+				sysTickWait1mS(100);
+				
+			  if (((.2*waitLimit) < waitTimer) && (waitTimer <= (.4*waitLimit)))
+				{
+					LEDBAR_TurnOff(4);
+				} else if (((.4*waitLimit) < waitTimer) && (waitTimer <= (.6*waitLimit)))
+				{
+					LEDBAR_TurnOff(3);
+				} else if (((.6*waitLimit) < waitTimer) && (waitTimer <= (.8*waitLimit)))
+				{
+					LEDBAR_TurnOff(2);
+				} else if (((.8*waitLimit) < waitTimer) && (waitTimer <= waitLimit))
+				{
+					LEDBAR_TurnOff(1);
+				}
 
 			}//end while(no input)
 			waitTimer = 0;
-			RIT128x96x4Clear();
+			LEDBAR_TurnOff(0);
+			LEDBAR_TurnOff(1);
+			LEDBAR_TurnOff(2);
+			LEDBAR_TurnOff(3);
+			LEDBAR_TurnOff(4);
+
 
 			// Was the input correct?
 			if (inputSelected == inputRequested)
@@ -361,15 +497,24 @@ main(void)
 					turnOn('G');
 					sysTickWait1mS(250);
 					turnOff('G');
+					turnOn('G');
+					sysTickWait1mS(250);
+					turnOff('G');
 
+					//Clear messages on the screen
+					RIT128x96x4Clear();
+				
 					// Display 'correct'
 					//Letters are displayed left to right
 					for(ii = 0; ii < 7;ii++)
 					{
-						correctMsg = correctcode[ii];
-						RIT128x96x4StringDraw(convert(correctMsg), xx + 6*ii,yy,15);
+						correct = correctcode[ii];
+						RIT128x96x4StringDraw(convert(correct), xx + 6*ii,yy,15);
 					}
 				sysTickWait1mS(1000);
+					
+				//Clear messages on the screen
+				RIT128x96x4Clear();
 			}
 			else
 			{
@@ -377,59 +522,67 @@ main(void)
 					turnOn('R');
 					sysTickWait1mS(250);
 					turnOff('R');
-					p1Dead = 1; ////THIS VALUE IS BOARD SPECIFIC
-					for(ii = 0 ; ii<8 ; ii++)
+					turnOn('R');
+					sysTickWait1mS(250);
+					turnOff('R');
+				
+				  //Clear messages on the screen
+					RIT128x96x4Clear();
+				
+					p1Dead = 1; 																																////THIS VALUE IS BOARD SPECIFIC
+					for(ii = 0 ; ii < 8 ; ii++)
 					{
 						loserMsg =  loserCodes[ii];
 						RIT128x96x4StringDraw(convert(loserMsg), xx + 6*ii,yy,15);
 					}
-					break;
-				}
+					sysTickWait1mS(1000);
+					//Clear messages on the screen
+					RIT128x96x4Clear();
+			}
 
-				//RESET VALUES
-				isInput = 0;
-				inputSelected = 0;
-
-			} //end for-loop number of turns
-
-				turnOn('B'); //meaning its time to pass
-			}//end while(not p1Dead)
+			//RESET VALUES
+			isInput = 0;
+			inputSelected = 0;
+			numTries = numTries +1;
+			}//end while(not dead && less than 5 turns)
 			
-			//Finish turn
+		turnOn('B');
+		sysTickWait1mS(250);
+		turnOff('B');
+		waitLimit = waitLimit*.9;
+		numTries = 0;
 			
-			countDead = p1Dead + p2Dead + p3Dead + p4Dead;
+			/************************************************/
+			
+			//END play game
+		
+			/************************************************/
+			
+			sendMessage[0] = (char) (p1Dead + 48);
+			sendMessage[1] = (char) (p2Dead + 48);
+			sendMessage[2] = (char) (p3Dead + 48);
+			sendMessage[3] = (char) (p4Dead + 48);
+			sendMessage[4] = (char) (2 + 48);																					//BOARD SPECIFIC!!!!
 	
-			//Send turn
-			Xbee_Send(turn);
-	
-			//Send num dead
-			Xbee_Send(countDead);
-			Xbee_Send(p1Dead);
-			Xbee_Send(p2Dead);
-			Xbee_Send(p3Dead);
-			Xbee_Send(p4Dead);
-	
+			Xbee_SendArray(sendMessage, 5);//Send Array to next board
+				
 		}
 		else if (countDead >= 3) {
 				if (!p1Dead){
 						//Display "P1 Wins!"
 							Display(13);
-							break;
 						}
 				else if (!p2Dead){
 							//Display "P2 Wins!"
 							Display(14);
-							break;
 						}
 				else if (!p3Dead){
 							//Display "P3 Wins!"
 							Display(15);
-							break;
 						}
 				else if (!p4Dead){
 							//Display "P4 Wins!"
 							Display(16);
-							break;
 						}
 				}
 			}
@@ -446,7 +599,7 @@ void Display(int numWin){
 		RIT128x96x4StringDraw(convertForWin(6), 27, 15, 15);
 		RIT128x96x4StringDraw(convertForWin(12), 33, 15, 15);
 		RIT128x96x4StringDraw(convertForWin(5), 39, 15, 15);
-		
+		RIT128x96x4StringDraw(convertForWin(31), 45, 15, 15);
 }
 
 char* convert(int baudotCode)
@@ -499,10 +652,10 @@ char* convertForWin(int baudotCode)
 		case(11): return "J";
 		case(12): return "N";		 
 		/////////////////////////////
-		case(13): return "1";					 	
-		case(14): return "2";
-		case(15): return "3";
-		case(16): return "4";
+		case(13): return "1"; 					 	
+		case(14): return "2"; 
+		case(15): return "3"; 
+		case(16): return "4"; 
 		////////////////////////////
 		case(17): return "Z";
 		case(18): return "L";
@@ -516,7 +669,7 @@ char* convertForWin(int baudotCode)
 		case(26): return "G";
 		case(28): return "M";
 		case(29): return "X";
-		case(31): return "V";
+		case(31): return "!";
 				
 	}
 	return " ";
